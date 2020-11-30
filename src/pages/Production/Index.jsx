116b40e9cb4474448e17406736e4facf50d4c451
LuCheng-git <1456096100@qq.com>
2020-11-30 11:12:22 +0800

    production static finish

--- src/pages/Production/Index.jsx
+++ src/pages/Production/Index.jsx
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 import PublicHeader from '../../components/PublicHeader/Index.jsx'
+import './Index.less'
 
 class Production extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {}
     }
-    render() { 
+    render() {
         return (
             <main className="productionContainer">
                 <PublicHeader title="首页"></PublicHeader>
+                <section className="proListContainer">
+                    <ul className="proListUl">
+                        <li className="proListItem">
+                            <div className="proSelect">
+                                <span className="iconfont icon-success_fill"></span>
+                                <span> Pdfssdfsdfsf</span>
+                            </div>
+
+                            <div className="proItemEdit">
+                                <span  className="iconfont icon-offline_fill"></span>
+                                <span  className="proNum">0</span>
+                                <span  className="iconfont icon-addition_fill"></span>
+                            </div>
+                        </li>
+                    </ul>
+                </section>
             </main>
         );
     }
 }
- 
+
 export default Production;
